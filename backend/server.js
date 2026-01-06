4f23d9af12400bc43ddfc1949b5788de6c444b0a
PratikParate <paratepratik10@gmail.com>
2026-01-06 19:48:41 +0530

    setup express server with cors/cookies, connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,27 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import connectDB from "./config/db";
 
 const app = express();
+connectDB();
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.get("/",(req,res)=> {
-    res.send("Hello World");
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.listen(process.env.PORT || 8080,(req,res)=> {
-    console.log('Server is running on port 8080');
-})
-
-
+app.listen(process.env.PORT || 8080, (req, res) => {
+  console.log("Server is running on port 8080");
+});
