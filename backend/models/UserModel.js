4694df2809249801e7fba7b5100ca84c6d7a249b
PratikParate <paratepratik10@gmail.com>
2026-01-07 19:58:10 +0530

    change Mongoose -> mongoose to resolve error

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -53,6 +53,6 @@ userSchema.methods.generateRefreshToken = function(){
 };
 
 
-const User = Mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
