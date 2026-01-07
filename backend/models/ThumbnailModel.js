ca4e794f8b44526323c73f6479de4c0b184f6eaa
PratikParate <paratepratik10@gmail.com>
2026-01-07 20:20:50 +0530

    add thumbnail model and Schema

---
+++ backend/models/ThumbnailModel.js
@@ -0,0 +1,71 @@
+import mongoose, { Schema } from "mongoose";
+
+const thumbnailSchema = new mongoose.Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    style: {
+      type: String,
+      required: true,
+      enum: [
+        "Bold & Graphic",
+        "Tech/Futuristic",
+        "Minimalist",
+        "Photorealistic",
+        "Illustrated",
+      ],
+    },
+    aspect_ratio: {
+      type: String,
+      required: true,
+      enum: ["16:9", "1:1", "9:16"],
+      default: "16:9",
+    },
+    color_scheme: {
+      type: String,
+      required: true,
+      enum: [
+        "vibrant",
+        "sunset",
+        "forest",
+        "neon",
+        "purple",
+        "monochrome",
+        "ocean",
+        "pastel",
+      ],
+    },
+    text_overlay: {
+      type: boolean,
+      default: false
+    },
+    image_url: {
+      type: String,
+      default: ''
+    },
+    prompt_used: {
+      type: String,
+    }, 
+    user_prompt: {
+      type: String,
+    },
+    isGenerating: {
+      type: boolean,
+      default: true
+    },
+  },
+  { timestamps: true }
+);
+
+const Thumbnail = mongoose.model("Thumbnail", thumbnailSchema);
+
+export default Thumbnail;
