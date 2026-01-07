8064a95a3778dcf8bab7be581a03c8dce4c5f3d5
PratikParate <paratepratik10@gmail.com>
2026-01-07 19:33:53 +0530

    add password hashing and JWT token methods to User schema

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -26,6 +26,32 @@ const userSchema = new mongoose.Schema(
   { timeStamps: true }
 );
 
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return null;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword,this.password);
+};
+
+userSchema.methods.generateAccessToken = function(){
+   return jwt.sign({
+    _id: this._id,
+    email: this.email
+  }, process.env.ACCESS_TOKEN_SECRET ,{
+    expiresIn: '15m'
+  });
+};
+
+userSchema.methods.generateRefreshToken = function(){
+   return jwt.sign({
+    _id: this._id,
+  }, process.env.REFRESH_TOKEN_SECRET ,{
+    expiresIn: '7d'
+  });
+};
+
 
 const User = Mongoose.model("User", userSchema);
 
