6b550a13e8af76f6a0694001832a4e84b96f3ec5
PratikParate <paratepratik10@gmail.com>
2026-01-07 19:27:58 +0530

    add user model with Schema

---
+++ backend/models/UserModel.js
@@ -0,0 +1,32 @@
+import mongoose, { Mongoose } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timeStamps: true }
+);
+
+
+const User = Mongoose.model("User", userSchema);
+
+export default User;
