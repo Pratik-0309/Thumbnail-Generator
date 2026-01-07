d015637a700f6e03eb42f9ca2f406f185c978db6
PratikParate <paratepratik10@gmail.com>
2026-01-07 19:24:29 +0530

    resolve ES module import error

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
-import connectDB from "./config/db";
+import connectDB from "./config/db.js";
 
 const app = express();
 connectDB();
