7b992a7f1adf8d7de6d61af2dc78b44c1925524d
PratikParate <paratepratik10@gmail.com>
2026-01-09 10:43:47 +0530

    add route for fetching thumbnails

--- backend/routes/thumbnailRoute.js
+++ backend/routes/thumbnailRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { generateThumbnail, deleteThumbnail } from '../controller/thumbnailController.js'
+import { generateThumbnail, deleteThumbnail, getUserThumnail, getSingleThumbnail } from '../controller/thumbnailController.js'
 import verifyJWT from '../middleware/authmiddleware.js'
 
 const router = express.Router();
 
 router.post('/generate',verifyJWT, generateThumbnail);
 router.delete('/delete/:id',verifyJWT, deleteThumbnail);
+router.get('/thumbnails',verifyJWT,getUserThumnail);
+router.get('/thumbnail/:id',verifyJWT,getSingleThumbnail);
 
 export default router;
